182dd2f30237ab8c4e037ee8141bd2a53c29c0bb
Student_hk <2236146406@qq.com>
2017-11-13 21:21:06 +0800

    Lab6w

---
+++ src/TestBridgeWord.java
@@ -0,0 +1,47 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestBridgeWord {
+
+	@Test
+	public void test1() {
+		String sourceText=ReadFromFile.readFileByChars("C:\\Users\\Tom\\"
+				+ "Desktop\\Code\\Lab6_Lab1w\\src\\Lab6_w_test.txt");
+		Main tmp = new Main();
+		Graph tmpGraph = tmp.createDirectedGraph(sourceText); 
+		String rst = tmp.queryBridgeWords(tmpGraph, "running", "he");
+		assertEquals("No running or he in the graph!",rst);
+	}
+	
+	@Test
+	public void test2() {
+		String sourceText=ReadFromFile.readFileByChars("C:\\Users\\Tom\\"
+				+ "Desktop\\Code\\Lab6_Lab1w\\src\\Lab6_w_test.txt");
+		Main tmp = new Main();
+		Graph tmpGraph = tmp.createDirectedGraph(sourceText);
+		String rst = tmp.queryBridgeWords(tmpGraph, "can", "well");
+		assertEquals("No bridge words from can to well!",rst);
+	}
+	
+	@Test
+	public void test3() {
+		String sourceText=ReadFromFile.readFileByChars("C:\\Users\\Tom\\"
+				+ "Desktop\\Code\\Lab6_Lab1w\\src\\Lab6_w_test.txt");
+		Main tmp = new Main();
+		Graph tmpGraph = tmp.createDirectedGraph(sourceText);
+		String rst = tmp.queryBridgeWords(tmpGraph, "no", "can");
+		assertEquals("The bridge words from no to can are: one.",rst);
+	}
+	
+	@Test
+	public void test4() {
+		String sourceText=ReadFromFile.readFileByChars("C:\\Users\\Tom\\"
+				+ "Desktop\\Code\\Lab6_Lab1w\\src\\Lab6_w_test.txt");
+		Main tmp = new Main();
+		Graph tmpGraph = tmp.createDirectedGraph(sourceText);
+		String rst = tmp.queryBridgeWords(tmpGraph, "who", "play");
+		assertEquals("The bridge words from who to play are: can, and should.",rst);
+	}
+
+}
